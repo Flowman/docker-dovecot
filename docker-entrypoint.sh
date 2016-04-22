7caee8debbd7b39e2e31d584e6513c20f77a31e2
Peter Szalatnay <theotherland@gmail.com>
2016-04-22 14:05:20 +0800

    ssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+while [ ! -f "/data/ssl/dh4096.pem" ]; do 
+   sleep 2
+done
+
 # if command starts with an option, prepend dovecot
 if [ "${1:0:1}" = '-' ]; then
     set -- dovecot "$@"
@@ -14,6 +18,11 @@ if [ "$1" = 'dovecot' ]; then
         echo mysql arugments required
         exit 1
     fi
+
+    # link in certs
+    mkdir /etc/dovecot/private
+    ln -s /data/ssl/server.crt /etc/dovecot/dovecot.pem 2> /dev/null
+    ln -s /data/ssl/server.key /etc/dovecot/private/dovecot.pem 2> /dev/null
 
     if [ ! -f "/etc/dovecot/dovecot-configured" ]; then
         echo connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASS >> /etc/dovecot/dovecot-sql.conf.ext

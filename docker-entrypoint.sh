ef1fa5bd80c6f3e714c337404dc20464741b3d0d
Peter Szalatnay <theotherland@gmail.com>
2016-04-26 12:18:36 +0800

    rancher fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,10 +24,10 @@ if [ "$1" = 'dovecot' ]; then
     ln -s /data/ssl/server.crt /etc/dovecot/dovecot.pem 2> /dev/null
     ln -s /data/ssl/server.key /etc/dovecot/private/dovecot.pem 2> /dev/null
 
-    if [ ! -f "/etc/dovecot/dovecot-configured" ]; then
-        echo connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASS >> /etc/dovecot/dovecot-sql.conf.ext
-        echo connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASS >> /etc/dovecot/dovecot-dict-sql.conf.ext
-        touch /etc/dovecot/dovecot-configured
+    if [ ! -f "/etc/dovecot/configured" ]; then
+        echo connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASSWORD >> /etc/dovecot/dovecot-sql.conf.ext
+        echo connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASSWORD >> /etc/dovecot/dovecot-dict-sql.conf.ext
+        touch /etc/dovecot/configured
     fi
 fi
 
